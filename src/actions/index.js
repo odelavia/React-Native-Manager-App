e91254b55e004bcf9d94f2dd73b3ea9768476273
odelavia <odelavia@gmail.com>
2018-06-07 12:54:27 -0400

    set up action to login user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,42 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
 
 export const emailChanged = (text) => {
   return {
-    type: 'EMAIL_CHANGED',
+    type: EMAIL_CHANGED,
     payload: text
   };
 };
 
 export const passwordChanged = (text) => {
   return {
-    type: 'PASSWORD_CHANGED',
+    type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({email, password}) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => loginUserSucess(dispatch, user))
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(user => loginUserSucess(dispatch, user))
+      .catch(() => loginUserFail(dispatch));
+    });
+  };
+};
+
+const loginUserFail = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_FAIL,
+    payload: user
+  });
+};
+
+const loginUserSucess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
