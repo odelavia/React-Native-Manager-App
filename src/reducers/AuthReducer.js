79167628b3363d4f3e3199aea0f8a485d3f6d2d3
odelavia <odelavia@gmail.com>
2018-06-07 13:04:42 -0400

    set up login fail case and error message

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,16 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '', user: null };
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  user: null,
+  error: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   console.log(action);
@@ -12,6 +22,8 @@ export default (state = INITIAL_STATE, action) => {
       return {...state, password: action.payload};
     case LOGIN_USER_SUCCESS:
       return {...state, user: action.payload};
+    case LOGIN_USER_FAIL:
+      return {...state, error: 'Authentication Failed.', password: ''};
     default:
       return state;
   }
